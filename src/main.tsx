00bf720f5aff0b868f3feeb7b1930f8b7388936a
Sokolcz97-rgb <swj87mvvg6@privaterelay.appleid.com>
2026-09-06 00:07:34 +0200

    style: load major reference matching pass

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./pages/app/community-reference-layout-fix.css";
 import "./pages/app/community-reference-pass3.css";
 import "./pages/app/community-reference-pass4.css";
 import "./pages/app/community-reference-pass5.css";
+import "./pages/app/community-reference-pass6.css";
 import "./i18n";
 
 const syncAppTitle = () => {
