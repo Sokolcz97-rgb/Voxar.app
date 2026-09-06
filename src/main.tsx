0ead86938e7c2f8a6d281e981577b248b35b8d1f
Sokolcz97-rgb <swj87mvvg6@privaterelay.appleid.com>
2026-09-06 13:34:24 +0200

    style: load reference pass v16 last

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import "./components/vox/reference/community-polish-v7.css";
 import "./components/vox/reference/community-topbar-polish-v10.css";
 import "./components/vox/reference/community-reference-convergence-v14.css";
 import "./components/vox/reference/community-reference-lock-v15.css";
+import "./components/vox/reference/community-reference-pass-v16.css";
 import "./i18n";
 
 const syncAppTitle = () => {
