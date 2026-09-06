99ee55f46f246982c65da2c43fe613ba93c60974
Sokolcz97-rgb <swj87mvvg6@privaterelay.appleid.com>
2026-09-06 14:24:43 +0200

    style: load v18 finishing layer

--- src/main.tsx
+++ src/main.tsx
@@ -17,6 +17,7 @@ import "./components/vox/reference/community-reference-lock-v15.css";
 import "./components/vox/reference/community-reference-pass-v16.css";
 import "./components/vox/reference/community-reference-bundle-v17.css";
 import "./components/vox/reference/community-reference-megapass-v18.css";
+import "./components/vox/reference/community-reference-megapass-v18-extra.css";
 import "./i18n";
 
 const syncAppTitle = () => {
