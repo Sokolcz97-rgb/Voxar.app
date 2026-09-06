9e4f7050102c2e9a11d08d0f50140fc7e2867991
Sokolcz97-rgb <swj87mvvg6@privaterelay.appleid.com>
2026-09-06 14:03:40 +0200

    load v17 reference bundle

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,7 @@ import "./components/vox/reference/community-topbar-polish-v10.css";
 import "./components/vox/reference/community-reference-convergence-v14.css";
 import "./components/vox/reference/community-reference-lock-v15.css";
 import "./components/vox/reference/community-reference-pass-v16.css";
+import "./components/vox/reference/community-reference-bundle-v17.css";
 import "./i18n";
 
 const syncAppTitle = () => {
